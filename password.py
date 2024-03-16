4418c441b045bf3e2a84aad83d42c433ba5601a8
TopNaid <habeebjinad6@gmail.com>
2024-03-16 09:43:57 +0100

    Simple Password checker

--- password.py
+++ password.py
@@ -2,11 +2,13 @@ print("WELCOME TO TOP BANK")
 print("Please enter your password! ")
 
 password = input("Password: ")
-x = ["&","?","^","/",".","[]","+",")","()","*","%","#","@",">","'","!","_","-"]
+x = ["&","?","^","/",".","[","]","+",")","(","*","%","#","@",">","'","!","_","-","$",",",":","|",";","=","<","}","{"]
+# Note: the condition for the user to have a strong password is that their password must include any
+#  of the special character above.
 for y in x:
     if y in password:
      print("Strong Password")
      break
 else: 
-    print("kindly enter a strong password or pick from the below suggestions\n1.thgf76hf'*$\n2.fggf888*&%:\n3. f7ffg^()fg!")
+    print("kindly enter a strong password or pick from the suggestions below\n1. thgf76hf'*$\n2. fggf888*&%:\n3. f7ffg^()fg!")
 
